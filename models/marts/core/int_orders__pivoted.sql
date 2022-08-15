ce6abe2ca581a2e77c6bc07a74b7fd9fbe22fde0
Andrew Dai <andyd52498@gmail.com>
2022-08-15 13:27:20 +0000

    jinja work

--- models/marts/core/int_orders__pivoted.sql
+++ models/marts/core/int_orders__pivoted.sql
@@ -1,23 +1,19 @@
+{%- set payment_methods = ['bank_transfer', 'credit_card', 'coupon', 'gift_card'] -%}
+
 with payments as (
-    select * from {{ ref('stg_payments') }}
+   select * from {{ ref('stg_payments') }}
 ),
 
-pivoted as (
-    select
-        order_id,
-        {% set payment_methods = ['bank_transfer','coupon','credit_card','gift_card'] %}
+final as (
+   select
+       order_id,
+       {% for payment_method in payment_methods -%}
 
-        {% for payment_method in payment_methods %}
+       sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount {% if not loop.last %} , {% endif %}
 
-        sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount
-
-        {% endfor %}
-
-        
-
-    from payments
-    where status <> 'fail'
-    group by 1
+       {% endfor -%}
+   from {{ ref('stg_payments') }}
+   group by 1
 )
 
-select * from pivoted
+select * from final
